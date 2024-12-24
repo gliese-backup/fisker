0f2b87d31d9b2a7950733512a8ff230e1d463257
gliese-backup <glieseb+1@proton.me>
2024-12-24 13:52:35 +0000

    Create home layout & components

--- app/page.js
+++ app/page.js
@@ -1,5 +1,16 @@
+"use client";
+import Header from "@/components/Header";
+import Preview from "@/components/Preview";
+import Configurator from "@/components/Configurator";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div onContextMenu={(e) => e.preventDefault()} className="min-h-screen">
+      <Header />
+      <Preview />
+      <Configurator />
+    </div>
+  );
 }
 
 export default Home;
