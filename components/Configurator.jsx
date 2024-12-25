cf0b7810f8524a7a12d11408d9d435d8167f9a25
gliese-backup <glieseb+1@proton.me>
2024-12-25 13:44:43 +0000

    Configurator Layout Ready

--- components/Configurator.jsx
+++ components/Configurator.jsx
@@ -10,10 +10,10 @@ function Configurator() {
       </p>
 
       {/* Exterior Color */}
-      <div>
-        <h3>Exterior Color</h3>
+      <div className="my-4">
+        <h3 className="mb-2 text-lg font-bold">Exterior Color</h3>
 
-        <div className="flex gap-1">
+        <div className="flex gap-2">
           {new Array(6).fill(0).map(
             (
               _,
@@ -21,22 +21,80 @@ function Configurator() {
             ) => (
               <button
                 key={i}
-                className={`${i === 0 && "ring"} rounded-full ring-blue-300 duration-100 hover:scale-110`}
+                className={`${i === 0 && "ring"} rounded-full ring-blue-300 duration-100 hover:scale-105`}
                 onClick={() => console.log(i + 1)}
               >
                 <Image
                   src="/sun_soaked.webp"
                   alt="Color"
-                  width={50}
-                  height={50}
+                  width={0}
+                  height={0}
+                  sizes="100%"
+                  className="w-12"
                 />
               </button>
             ),
           )}
         </div>
       </div>
+
       {/* Wheel Color */}
+      <div className="my-4">
+        <h3 className="mb-2 text-lg font-bold">Wheel Options</h3>
+
+        <div className="flex gap-2">
+          {new Array(3).fill(0).map(
+            (
+              _,
+              i, // 6 colors
+            ) => (
+              <button
+                key={i}
+                className={`${i === 0 && "ring"} rounded-full ring-blue-300 duration-100 hover:scale-105`}
+                onClick={() => console.log(i + 1)}
+              >
+                <Image
+                  src="/sun_soaked.webp"
+                  alt="Color"
+                  width={0}
+                  height={0}
+                  sizes="100%"
+                  className="w-14"
+                />
+              </button>
+            ),
+          )}
+        </div>
+      </div>
+
       {/* Interior Color */}
+      <div className="my-4">
+        <h3 className="mb-2 text-lg font-bold">Interior Color</h3>
+
+        <div className="flex gap-2">
+          {new Array(2).fill(0).map(
+            (
+              _,
+              i, // 6 colors
+            ) => (
+              <button
+                key={i}
+                className={`${i === 0 && "ring"} rounded-full ring-blue-300 duration-100 hover:scale-105`}
+                onClick={() => console.log(i + 1)}
+              >
+                <Image
+                  src="/sun_soaked.webp"
+                  alt="Color"
+                  width={0}
+                  height={0}
+                  sizes="100%"
+                  className="w-14"
+                />
+              </button>
+            ),
+          )}
+        </div>
+      </div>
     </>
   );
 }
