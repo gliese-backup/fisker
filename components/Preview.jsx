f9b680747d7552a961da02c5111b100919488c88
gliese-backup <glieseb+1@proton.me>
2024-12-25 14:28:37 +0000

    Add React Medium Image Zoom effect

--- components/Preview.jsx
+++ components/Preview.jsx
@@ -1,17 +1,21 @@
 import Image from "next/image";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 function Preview() {
   return (
     <>
       {/* Exterior Image */}
-      <Image
-        src="/slipstream_black.webp"
-        alt=""
-        width={0}
-        height={0}
-        sizes="100%"
-        className="h-96 w-full rounded-lg object-cover sm:h-[500px]"
-      />
+      <Zoom>
+        <Image
+          src="/slipstream_black.webp"
+          alt=""
+          width={0}
+          height={0}
+          sizes="100%"
+          className="h-96 w-full rounded-lg object-cover sm:h-[500px]"
+        />
+      </Zoom>
 
       {/* Exterior Description */}
       <section className="my-4 pl-2">
@@ -19,14 +23,19 @@ function Preview() {
         <p>A rich and luxurious interior with a modern and sleek design.</p>
       </section>
       {/* Interior Image */}
-      <Image
-        src="/slipstream_black.webp"
-        alt=""
-        width={0}
-        height={0}
-        sizes="100%"
-        className="h-96 w-full rounded-lg object-cover sm:h-[500px]"
-      />
+      <Zoom>
+        <Image
+          src="/slipstream_black.webp"
+          alt=""
+          width={0}
+          height={0}
+          sizes="100%"
+          className="h-96 w-full rounded-lg object-cover sm:h-[500px]"
+        />
+      </Zoom>
+      <section className="mt-4 pl-2">
+        <h3 className="text-xl font-bold">Interior Color</h3>
+      </section>
     </>
   );
 }
